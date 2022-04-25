1818d17de2e6657036931a9b56c3d564f290ae51
angelo-squadra <angelo.savioti@squadra.com.br>
2022-04-25 16:27:33 -0300

    created base structure

---
+++ lib/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:study_flutter_push_notifications/app/pages/home/home_page.dart';
+import 'package:study_flutter_push_notifications/app/pages/notifications/notifications_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/notifications': (context) => const NotificationsPage(),
+      },
+    );
+  }
+}
