934ec2bffbd736c7b2d2be5cd4de22df54feccc2
angelo-squadra <angelo.savioti@squadra.com.br>
2022-04-25 17:22:36 -0300

    added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:study_flutter_push_notifications/app/app.dart';
 
 void main() {
+  Firebase.initializeApp();
   runApp(const App());
 }
